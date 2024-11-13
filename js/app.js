e758bd01751d9fc2524952c8509a4e9a7360be7d
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-13 10:23:17 -0300

    chore: add function limpar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let valorGeral = 0;
+
 function adicionar() {
   //recuperar valores: nome do produto, quantidade e valor.
   let nomeProduto = document.getElementById('produto').value;
@@ -5,14 +7,24 @@ function adicionar() {
   let valor = nomeProduto.split('R$')[1];
   let qtdProduto = document.getElementById('quantidade').value;
 
-  let total = valor * qtdProduto;
-
-  alert(total)
   // calcular o preço, o nosso subtotal.
+  let total = valor * qtdProduto;
+  
   //adicionar no carrinho
+  let carrinho = document.getElementById('lista-produtos');
+  carrinho.innerHTML = carrinho.innerHTML +  `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${qtdProduto}x</span> ${produto} <span class="texto-azul">R$${valor}</span>
+        </section>`
   //atualizar o valor total
+  valorGeral = valorGeral + total;
+  let campoTotal = document.getElementById('valor-total');
+  campoTotal.textContent = `R$ ${valorGeral}`
 }
 
 function limpar() {
+  valorGeral = '0'
+  document.getElementById('lista-produtos').innerHTML = '';
+  document.getElementById('valor-total').innerHTML = ' ';
+ 
 
 }
